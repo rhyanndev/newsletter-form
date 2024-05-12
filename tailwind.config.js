772c0421720f4b56c7e8708c05d7ab8c8455ec07
rhyanndev <rhyannjosebezerra@gmail.com>
2024-05-12 20:13:49 -0300

    add: Add files in repository

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+//Dizer quais arquivos que ele precisa monitorar que tem o tailwind
+
+export default {
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins:[],
+};
